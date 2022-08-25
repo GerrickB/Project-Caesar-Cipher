fe7a5c60bab111ce1083c08617013eeab596680f
Gerrick B <lsgenether@gmail.com>
2022-08-25 03:53:39 -0700

    Add code to function

--- cipher.rb
+++ cipher.rb
@@ -1,11 +1,12 @@
-# str.split
-# ["hello", "there"]
+# str = "hello there"
 # str.split("")
 # ["h", "e", "l", "l", "o", " ", "t", "h", "e", "r", "e"]
 # use .next or .next!
 
 def caesar_cipher(string)
     string_array = string.split("")
+    string_array = string_array.filter_map { |string| string.next unless string == " " }
+    print string_array
 end
 
 caesar_cipher('hello world')
