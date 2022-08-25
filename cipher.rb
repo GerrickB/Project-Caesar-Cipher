8c99e02372d121046cb2804631ad8070b8f9b227
Gerrick B <lsgenether@gmail.com>
2022-08-24 21:17:26 -0700

    Initialize caesar_cipher method

--- cipher.rb
+++ cipher.rb
@@ -3,3 +3,9 @@
 # str.split("")
 # ["h", "e", "l", "l", "o", " ", "t", "h", "e", "r", "e"]
 # use .next or .next!
+
+def caesar_cipher(string)
+    string_array = string.split("")
+end
+
+caesar_cipher('hello world')
