4a1935e331773823bb3099136ef7fe4ae13ff7ef
Gerrick B <lsgenether@gmail.com>
2022-08-25 19:46:41 -0700

    Modify caesar_cipher function

--- cipher.rb
+++ cipher.rb
@@ -2,11 +2,24 @@
 # str.split("")
 # ["h", "e", "l", "l", "o", " ", "t", "h", "e", "r", "e"]
 # use .next or .next!
+# Not woriking: string_array = string_array.filter_map { |string| string.next unless string == " " }
 
-def caesar_cipher(string)
+def caesar_cipher(string, shift)
     string_array = string.split("")
-    string_array = string_array.filter_map { |string| string.next unless string == " " }
+
+    shift.times do
+        new_array = []
+        string_array.each do |string|
+            if string != " "
+                new_array.push(string.next)
+            else
+                new_array.push(string)
+            end
+        end
+        string_array = new_array 
+    end
+
     print string_array
 end
 
-caesar_cipher('hello world')
+caesar_cipher('computer', 7)
